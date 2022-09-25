44bd35039e4041f79d90763e8f0f6e11ddc12737
Moetez <moetezmaddouri@gmail.com>
2022-09-25 14:48:39 +0200

    exercices of static and list , lambda expression

--- src/JavaStreams/propramePrincipal.java
+++ src/JavaStreams/propramePrincipal.java
@@ -13,23 +13,24 @@ public class propramePrincipal {
 		// TODO Auto-generated method stub
 	List<Person> people =getPeople();
 	
+	//map
 	System.out.println("*********************** map : get list person age desc ******************");
 	List<Integer> listpersonAge= people.stream().map(x->x.getAge()).sorted().collect(Collectors.toList());
 	System.out.println(listpersonAge);
 	
-	
+	//filter
 	System.out.println("*********************** filter les person female ******************");
 	List<Person> filtredFemalPerson=people.stream().filter(p->p.getGender().equals(Gender.FEMALE)).collect(Collectors.toList());
 	filtredFemalPerson.forEach(System.out::println);
 	System.out.println();
 	
-	
+	//Sorted
 	System.out.println("*********************** sorted les person female ******************");
 	List<Person> sotedPersonByAge=people.stream().sorted(Comparator.comparing(Person::getAge).reversed()).collect(Collectors.toList());
 	sotedPersonByAge.forEach(System.out::println);
 	System.out.println();
 	
-	
+	// All match , anyMatch,noneMatch()
 	System.out.println("*********************** All match , anyMatch,noneMatch veif condition in list ******************");
 	Boolean existInAll=people.stream().allMatch(p->p.getAge()>30);
 	System.out.println("all person age >30 : "+existInAll);
@@ -39,17 +40,17 @@ public class propramePrincipal {
 	System.out.println("there is no person with age < 5 : "+notExistSpecific);
 	System.out.println();
 	
-	
+	// max
 	System.out.println("*********************** Max Min in list ******************");
 	Person max=people.stream().max(Comparator.comparing(Person::getAge)).get();
 	System.out.println(max.getAge());
 	
-	
+	// Factoriel with reduce
 	System.out.println("*********************** Factoriell with lambda expression  ******************");
 	System.out.println(getFactoriel(5));
 	System.out.println();
 	
-	
+	// GroupingBy
 	System.out.println("*********************** groupedBy lambda expression  ******************");
 	Map<Gender,List<Person>> groupedpeople= people.stream().collect(Collectors.groupingBy(Person::getGender));
 	groupedpeople.forEach((gender,listPerson)->{
@@ -70,6 +71,7 @@ public class propramePrincipal {
 		        new Person("Zelda Brown", 120, Gender.FEMALE)
 		    );
 		  }
+	 //Reduce
 	 private static Integer getFactoriel(int x) {
 		return  IntStream.range(1, x+1).reduce(1, (r,z)->r*z);
 	 }
